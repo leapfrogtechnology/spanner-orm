9aeef13871849b89aec2067ccf63192a37d6a7f7
SanishMaharjan <sanishmaharjan@lftechnology.com>
2018-07-01 13:14:32 +0545

    field validation, build count query

--- spannerorm/base_model.py
+++ spannerorm/base_model.py
@@ -19,19 +19,78 @@ class BaseModel(object):
         for key in kwargs.iterkeys():
             self.__setattr__(key, kwargs.get(key))
 
-    def __str__(self):
-        object_str = {}
-        for key, value in inspect.getmembers(self.__class__, Helper.is_property):
-            object_str[key] = self.__getattribute__(key)
+        self.__state__ = self.__class__.State()
 
-        return object_str.__str__()
+    def __str__(self):
+        return Helper.get_model_props(self).__str__()
 
     def __dict__(self):
-        model_data = {}
-        for key, value in inspect.getmembers(self.__class__, Helper.is_property):
-            model_data[key] = self.__getattribute__(key)
+        return Helper.get_model_props(self)
 
-        return model_data
+    def equals(self, obj):
+        if isinstance(BaseModel, obj) is False:
+            return False
+
+        return Helper.get_model_props(self) == Helper.get_model_props(obj)
+
+    def _state(self):
+        return self.__state__
+
+    def is_new_record(self):
+        return self.__state__.is_new
+
+    def get_errors(self):
+        return self.__state__.errors
+
+    def before_save(self):
+        """
+        Overwrite method on implementation class that execute before save
+        """
+        return
+
+    def before_update(self):
+        """
+        Overwrite method on implementation class that execute before update
+        """
+        return
+
+    def after_save(self):
+        """
+        Overwrite method on implementation class that execute after save
+        """
+        return
+
+    def after_update(self):
+        """
+        Overwrite method on implementation class that execute after update
+        :return:
+        """
+        return
+
+    def validate(self):
+        """
+        Validate model properties
+
+        :rtype: bool
+        :return: True if model properties are valid else False
+        """
+        for key, value in inspect.getmembers(self.__class__, Helper.is_property):
+            if self.validate_property(value) is False:
+                return False
+
+        return True
+
+    def validate_property(self, prop):
+        """
+        Validate model property
+
+        :type prop: property
+        :param prop: model property
+
+        :rtype: bool
+        :return:
+        """
+        return Helper.validate_model_prop(self, prop)
 
     @classmethod
     def _meta(cls):
@@ -53,6 +112,44 @@ class BaseModel(object):
         """
         results = Executor.execute_query(query_string, query_builder.params, query_builder.param_types)
         return DataParser.map_model(results, query_builder.select_cols, cls)
+
+    @classmethod
+    def count(cls, criteria=None):
+        """
+        return count
+
+        :type criteria: Criteria
+        :param criteria:
+
+        :rtype: int
+        :return: count rows
+        """
+        if criteria is None:
+            criteria = Criteria()
+
+        query_builder = QueryBuilder(cls, criteria)
+        query_string = query_builder.get_count()
+        result = Executor.execute_query(query_string, query_builder.params, query_builder.param_types)
+
+        return result.one()[0]
+
+    @classmethod
+    def delete_one(cls, criteria):
+        # Todo
+        id_list = [('3')]
+        Executor.delete_data(cls._meta().table_name, id_list)
+
+    @classmethod
+    def delete_by_pk(cls, pk):
+        pass
+
+    @classmethod
+    def delete_all(cls, criteria=None):
+        pass
+
+    @classmethod
+    def is_exist(cls, criteria):
+        pass
 
     @classmethod
     def find(cls, criteria=None):
@@ -78,29 +175,105 @@ class BaseModel(object):
         else:
             return None
 
-    # Todo
-    def insert(self):
-        prepare_data = PrepareData(self._meta().db_table)
+    @classmethod
+    def find_by_pk(cls, pk):
+        pass
+
+    @classmethod
+    def find_all(cls, criteria=None):
+        pass
+
+    @classmethod
+    def get_meta_data(cls):
+        pass
+
+    @classmethod
+    def get_primary_key_value(cls):
+        pass
+
+    @classmethod
+    def has_property(cls, property_name):
+        pass
+
+    @classmethod
+    def insert(cls, columns, data):
+        # Todo
+        prepare_data = PrepareData(cls._meta().db_table)
         insert_data = prepare_data.get_data()
         columns = ('id', 'Active', 'Name', 'Subdomain')
-        Executor.insert_data(self._meta().db_table, columns, insert_data)
+        Executor.insert_data(cls._meta().db_table, columns, insert_data)
 
-    # Todo
     @classmethod
-    def save(cls):
+    def primary_key(cls):
+        pass
+
+    @classmethod
+    def relations(cls):
+        pass
+
+    @classmethod
+    def validation_rules(cls):
+        pass
+
+    @classmethod
+    def attr_validation_rules(cls):
+        pass
+
+    @classmethod
+    def save(cls, model_obj):
+        # Todo
         prepare_data = PrepareData(cls._meta().db_table)
         insert_data = prepare_data.get_data()
         columns = ('id', 'name', 'address', 'points', 'is_active', 'join_date', 'modified_at')
         Executor.save_data(cls._meta().db_table, columns, insert_data)
 
-    # Todo
-    def update(self):
-        prepare_data = PrepareData(self._meta().table_name)
+    @classmethod
+    def save_all(cls, model_obj_list):
+        pass
+
+    @classmethod
+    def table_name(cls):
+        pass
+
+    @classmethod
+    def update(cls, columns, data):
+        # Todo
+        prepare_data = PrepareData(cls._meta().table_name)
         insert_data = prepare_data.get_data()
         columns = ('id', 'Active', 'Name', 'Subdomain')
-        Executor.update_data(self._meta().table_name, columns, insert_data)
+        Executor.update_data(cls._meta().table_name, columns, insert_data)
 
-    # Todo
-    def delete(self):
-        id_list = [('3')]
-        Executor.delete_data(self._meta().table_name, id_list)
+    @classmethod
+    def update_by_pk(cls, pk, data):
+        pass
+
+    @classmethod
+    def update_all(cls, criteria, data):
+        pass
+
+    @classmethod
+    def with_relation(cls):
+        pass
+
+    class State(object):
+        _is_new = True
+        _errors = {}
+
+        @property
+        def is_new(self):
+            return self._is_new
+
+        @is_new.setter
+        def is_new(self, is_new):
+            if isinstance(is_new, bool) is False:
+                raise TypeError('Should be bool type')
+            self._is_new = is_new
+
+        @property
+        def errors(self):
+            return self._errors
+
+        @errors.setter
+        def errors(self, errors):
+            self._errors = errors
+
--- spannerorm/helper.py
+++ spannerorm/helper.py
@@ -1,6 +1,8 @@
-import base_model
+import re
 import inspect
+import base_model
 from .dataType import *
+from datetime import date
 
 
 class Helper(object):
@@ -27,6 +29,23 @@ class Helper(object):
         return isinstance(v, StringField) | isinstance(v, IntegerField) | isinstance(v, BoolField) \
                | isinstance(v, IntegerField) | isinstance(v, FloatField) | isinstance(v, BytesField) \
                | isinstance(v, DateField) | isinstance(v, TimeStampField) | isinstance(v, EnumField)
+
+    @classmethod
+    def get_model_props(cls, model_obj):
+        """
+        Return model props key-value
+
+        :type model_obj: base_model.BaseModel
+        :param model_obj: model
+
+        :rtype: dict
+        :return:
+        """
+        model_props = {}
+        for key, value in inspect.getmembers(model_obj.__class__, Helper.is_property):
+            model_props[key] = model_obj.__getattribute__(key)
+
+        return model_props
 
     @classmethod
     def get_model_attrs(cls, model_cls):
@@ -68,3 +87,202 @@ class Helper(object):
             raise TypeError('Criteria model property {} not exist'.format(model_attr_name))
 
         return model_attrs.get(model_attr_name)
+
+    @classmethod
+    def validate_model_prop(cls, model_cls, prop):
+        """
+        Validate model attr
+
+        :type model_cls: base_model.BaseModel
+        :param model_cls: Model class
+
+        :type prop: property
+        :param prop:
+
+        :rtype: bool
+        :return:
+        """
+        if isinstance(prop, property) is False:
+            raise TypeError('Invalid object property')
+
+        model_attr_name = '_' + prop.fget.__name__
+        model_attrs = Helper.get_model_attrs(model_cls)
+
+        if model_attrs.has_key(model_attr_name):
+            attr = model_attrs.get(model_attr_name)
+            if isinstance(attr, IntegerField) or isinstance(attr, FloatField):
+                return Helper.validate_number_field(attr.value, max_value=attr.max_value, min_value=attr.min_value,
+                                                    null=attr.null)
+            elif isinstance(attr, StringField):
+                return Helper.validate_string_field(attr.value, max_length=attr.max_length, reg_exr=attr.reg_exr,
+                                                    null=attr.null)
+            elif isinstance(attr, BoolField):
+                return Helper.validate_bool_field(attr.value, null=attr.null)
+            elif isinstance(attr, TimeStampField):
+                return Helper.validate_timestamp_field(attr.value, null=attr.null)
+            elif isinstance(attr, DateField):
+                return Helper.validate_date_field(attr.value, null=attr.null)
+            elif isinstance(attr, EnumField):
+                return Helper.validate_enum_field(attr.value, enum_list=attr.enum_list, null=attr.null)
+
+        return True
+
+    @classmethod
+    def validate_number_field(cls, value, max_value=None, min_value=None, null=True):
+        """
+        Validate number field value
+
+        :type value: int | float
+        :param value: integer value
+
+        :type max_value: int
+        :param max_value: max allow number value
+
+        :type min_value: int
+        :param min_value: min allow integer value
+
+        :type null: bool
+        :param null: is allow None value
+
+        :rtype: bool
+        :return: True if valid else False
+        """
+        if null is False and value is None:
+            return False
+
+        if value is not None:
+            if isinstance(value, int) is False:
+                return False
+
+            if max_value is not None and value > max_value:
+                return False
+
+            if min_value is not None and value < min_value:
+                return False
+
+        return True
+
+    @classmethod
+    def validate_string_field(cls, value, max_length=None, reg_exr=None, null=True):
+        """
+        Validate string field value
+
+        :type max_length: int
+        :param max_length: max allow string lenght
+
+        :type reg_exr: str
+        :param reg_exr: regex pattern
+
+        :type null: bool
+        :param null: is allow None value
+
+        :rtype: bool
+        :return: True if valid else False
+        """
+        if null is False and (value is None or value.strip() == ''):
+            return False
+
+        if value is not None:
+            if isinstance(value, str) is False:
+                return False
+
+            if max_length is not None and len(value) > max_length:
+                return False
+            if reg_exr is not None:
+                pattern = re.compile(reg_exr)
+                if pattern.match(value) is None:
+                    return False
+
+        return True
+
+    @classmethod
+    def validate_bool_field(cls, value, null=True):
+        """
+        Validate bool field value
+
+        :type value: bool
+        :param value:
+
+        :type null: bool
+        :param null: is allow None value
+
+        :rtype: bool
+        :return: True if valid else False
+        """
+        if null is False and value is None:
+            return False
+
+        if value is not None and isinstance(value, bool) is False:
+            return False
+
+        return True
+
+    @classmethod
+    def validate_timestamp_field(cls, value, null=True):
+        """
+        Validate timestamp field value
+
+        :type value: int | float
+        :param value:
+
+        :type null: bool
+        :param null: is allow None value
+
+        :rtype: bool
+        :return: True if valid else False
+        """
+        if null is False and value is None:
+            return False
+
+        if value is not None and isinstance(value, int) is False and isinstance(value, float) is False:
+            return False
+
+        return True
+
+    @classmethod
+    def validate_date_field(cls, value, null=True):
+        """
+        Validate enum field value
+
+        :type value: date
+        :param value:
+
+        :type null: bool
+        :param null: is allow None value
+
+        :rtype: bool
+        :return: True if valid else False
+        """
+        if null is False and value is None:
+            return False
+
+        if value is not None and isinstance(value, date) is False:
+            return False
+
+        return True
+
+    @classmethod
+    def validate_enum_field(cls, value, enum_list, null=True):
+        """
+        Validate enum field value
+
+        :type value: object
+        :param value:
+
+        :type enum_list: list
+        :param enum_list: list of allow value
+
+        :type null: bool
+        :param null: is allow None value
+
+        :rtype: bool
+        :return: True if valid else False
+        """
+        if null is False and value is None:
+            return False
+
+        if value is not None:
+            if value in enum_list is False:
+                return False
+
+        return True
