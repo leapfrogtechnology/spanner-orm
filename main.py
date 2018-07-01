c5b26aaa657c6ee6c078fbfc333a7e3d525b37ac
SanishMaharjan <sanishmaharjan@lftechnology.com>
2018-07-01 18:11:10 +0545

    check delete model functions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from time import time
 from spannerorm import Connection, Criteria, ModelJSONEncoder
 from datetime import date
+import logging
 
 from models import Temp
 
@@ -10,7 +11,6 @@ app = Flask(__name__)
 app.json_encoder = ModelJSONEncoder
 
 Connection.config('develop', 'auth')
-
 
 # criteria = Criteria()
 # criteria.add_condition((Temp.name, '=', 'Sanish Maharjan'))
@@ -25,6 +25,7 @@ temp.join_date = date(2000, 10, 10)
 print(temp.validate())
 print(temp.get_errors())
 
+
 @app.route('/get')
 def get_records():
     start_time = time()
@@ -37,5 +38,18 @@ def get_records():
     return jsonify(temp)
 
 
+@app.route('/delete')
+def delete_record():
+    start_time = time()
+    criteria = Criteria()
+    criteria.add_condition((Temp.name, 'LIKE', 'Pradeep%'))
+    Temp.delete_all(criteria)
+    print("--- %s Application Execution time ---" % (time() - start_time))
+
+    return jsonify('success')
+
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logging.error('This message should go to the log file')
     app.run(host='0.0.0.0', port=8282, debug=True)
