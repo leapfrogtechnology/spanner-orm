5892ac01c05bc4360eb69de199f132bf0d6a1127
SanishMaharjan <sanishmaharjan@lftechnology.com>
2018-07-02 22:56:06 +0545

    test file updating

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from time import time
 from spannerorm import Connection, Criteria, ModelJSONEncoder
 from datetime import date
 import logging
+from uuid import uuid4
+from spannerorm import DataParser
 
 from models import Temp
 
@@ -38,7 +40,7 @@ def get_all_records():
     criteria = Criteria()
     criteria.add_condition((Temp.is_active, '=', True))
     criteria.limit = 2
-    #criteria.add_condition((Temp.join_date, '=', date(2018, 02, 20)))
+    # criteria.add_condition((Temp.join_date, '=', date(2018, 02, 20)))
     temps = Temp.find_all(criteria)
     print("--- %s Application Execution time ---" % (time() - start_time))
 
@@ -55,6 +57,73 @@ def delete_record():
 
     return jsonify('success')
 
+
+@app.route('/insert')
+def insert_record():
+    start_time = time()
+    data_list = [{
+        "id": str(uuid4()),
+        "is_active": True,
+        "join_date": date(2018,02,20),
+        "name": "Dinesh Thapa",
+        "points": 150,
+    }, {
+        "address": "kalanki, Kathmandu",
+        "id": str(uuid4()),
+        "join_date": date(2018,02,20),
+        "name": "Rasna Shakya",
+        "points": 200
+    }]
+
+    temps = Temp.insert_block(data_list)
+    print("--- %s Application Execution time ---" % (time() - start_time))
+
+    return jsonify(temps)
+
+@app.route('/save')
+def save_record():
+    start_time = time()
+    temp = Temp.find_by_pk('8921e454-7161-44af-9b8b-1b84f81a22bc')
+    temp.name = 'Rammu Cha'
+    temp.is_active = False
+    print(temp)
+    response = Temp.save(temp)
+    print("--- %s Application Execution time ---" % (time() - start_time))
+
+    return jsonify(response)
+
+@app.route('/updateByPk')
+def update__by_pk():
+    start_time = time()
+    data = {
+        "is_active": True,
+        "name": "Ram Maya Thapa",
+        "points": 150,
+    }
+
+    response = Temp.update_by_pk('8921e454-7161-44af-9b8b-1b84f81a22bc', data)
+    print("--- %s Application Execution time ---" % (time() - start_time))
+
+    return jsonify(response)
+
+@app.route('/test')
+def test():
+    data_list = [{
+        "id": '8921e454-7161-44af-9b8b-1b84f81a22bc',
+        "is_active": True,
+        "join_date": date(2018, 02, 20),
+        "name": "Sanish Thapa",
+        "points": 150,
+    }, {
+        "address": "kalanki, Kathmandu",
+        "id": 'a11b77d2-827d-41e3-938b-9022174be22d',
+        "join_date": date(2018, 02, 20),
+        "name": "Rasna Shakya",
+        "points": 200
+    }]
+
+    DataParser.parse_update_raw_data(Temp, data_list, False)
+    return 'success'
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
