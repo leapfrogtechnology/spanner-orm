07a6e17dffa754a350ff0f64521d3d3a7a6bac33
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2018-07-09 15:44:02 +0545

    update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -9,20 +9,20 @@ CREATE TABLE sample (
 ) PRIMARY KEY (id)
 
 CREATE TABLE users (
-	id STRING(MAX) NOT NULL,
-	acl STRING(MAX),
-	created_at TIMESTAMP,
-	created_by STRING(MAX),
-	email STRING(MAX),
-	is_deleted BOOL,
-	name STRING(MAX),
-	organization_id STRING(MAX),
-	password STRING(MAX),
-	phone_number STRING(20),
-	photo_url STRING(MAX),
-	role_id STRING(MAX),
-	updated_at TIMESTAMP,
-	updated_by STRING(MAX),
+    id STRING(MAX) NOT NULL,
+    acl STRING(MAX),
+    created_at TIMESTAMP,
+    created_by STRING(MAX),
+    email STRING(MAX),
+    is_deleted BOOL,
+    name STRING(MAX),
+    organization_id STRING(MAX),
+    password STRING(MAX),
+    phone_number STRING(20),
+    photo_url STRING(MAX),
+    role_id STRING(MAX),
+    updated_at TIMESTAMP,
+    updated_by STRING(MAX),
 ) PRIMARY KEY (id)
 
 CREATE TABLE organizations (
@@ -32,10 +32,20 @@ CREATE TABLE organizations (
 	created_by STRING(MAX),
 	is_deleted BOOL,
 	logo STRING(MAX),
-	organization_name STRING(MAX),
+	name STRING(MAX),
 	state STRING(MAX),
 	sub_domain STRING(MAX),
 	updated_at TIMESTAMP,
 	updated_by STRING(MAX),
 	zip_code STRING(20),
+) PRIMARY KEY (id)
+
+CREATE TABLE roles (
+	id STRING(MAX) NOT NULL,
+	created_at TIMESTAMP,
+	created_by STRING(MAX),
+	is_deleted BOOL,
+	name STRING(50),
+	updated_at TIMESTAMP,
+	updated_by STRING(MAX),
 ) PRIMARY KEY (id)
