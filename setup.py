66fcc3abe7e52f00ada5c423c2c09f7c0eba121d
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2018-07-19 15:48:02 +0545

    fix add criteria query builder issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spannerorm",
-    version="0.0.5",
+    version="0.0.6",
     author="Sanish Maharjan",
     author_email="sanishmaharjan@lftechnology.com",
     description="ORM for cloud spanner",
