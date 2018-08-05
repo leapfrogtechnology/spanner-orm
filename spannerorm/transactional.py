e265ec7b3d0b8be15fd48433c42878dd1ada905a
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2018-08-05 17:24:52 +0545

    Retry on transaction aborted

--- spannerorm/transactional.py
+++ spannerorm/transactional.py
@@ -1,3 +1,5 @@
+import time
+import logging
 from functools import wraps
 from .connection import Connection
 from .spanner_exception import SpannerException
@@ -8,28 +10,41 @@ from google.api_core.exceptions import Aborted, GoogleAPICallError
 def transactional(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
+        return execute_transaction(func, *args, **kwargs)
+
+    return wrapper
+
+
+def execute_transaction(func, *args, **kwargs):
+    max_retry = 10
+    while max_retry:
+        max_retry -= 1
         db = Connection.get_instance()
         with SessionCheckout(db._pool) as session:
             transaction = session._transaction
             if transaction is None:
                 transaction = session.transaction()
-
             if transaction._transaction_id is None:
                 transaction.begin()
-
         try:
             response = func(transaction=transaction, *args, **kwargs)
+            if max_retry:
+                raise Aborted('Fail to process')
             transaction.commit()
-
             return response
-        except Aborted as exc:
+        except Aborted as error:
             del transaction
-            raise SpannerException('Transaction Aborted')
-        except GoogleAPICallError:
+            if max_retry:
+                logging.warn('Transaction Aborted; Will Retry in 1 sec...')
+                time.sleep(1)
+            else:
+                logging.error(error)
+                raise SpannerException('Transaction Aborted')
+        except GoogleAPICallError as error:
             del transaction
+            logging.error(error)
             raise SpannerException('Spanner Db Api Call Error')
-        except Exception:
+        except Exception as error:
             transaction.rollback()
-            raise
-
-    return wrapper
+            logging.error(error)
+            raise error
